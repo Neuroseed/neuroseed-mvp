3aab52ee693de3544c1df1b7d7f70c79db34c239
Stanislav <stas.kozhemyako@gmail.com>
2018-03-05 12:19:34 +0200

    Edited tasks

--- worker/tasks.py
+++ worker/tasks.py
@@ -2,12 +2,15 @@ import celery
 from .app import app
 
 
-@app.task
-def get_datasets():
-    return {'datasets': []}
+@app.task(name='train_model')
+def train_model(id):
+    return {'id': id}
 
+@app.task(name='test_model')
+def test_model(id):
+    return {'id': id}
 
-@app.task
-def get_models():
-    return {'models': []}
+@app.task(name='predict_model')
+def predict_model(id):
+    return {'id': id}
 
