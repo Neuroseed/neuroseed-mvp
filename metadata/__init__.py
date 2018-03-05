5c53eabc26d41f65ec82c34aeed47f30d4279b02
Ihor Omelchenko <counter3d@gmail.com>
2018-03-05 12:41:39 +0200

    UPD: use mongodb's pymodm for metadata
    Added metadata odm for dataset, architecture, model, task. Changed metadata_config.json.

--- metadata/__init__.py
+++ metadata/__init__.py
@@ -1,1 +1,18 @@
-from .metadata import *
+import json
+
+from pymodm import connect
+
+from .dataset import *
+from .architecture import *
+from .model import *
+from .task import *
+
+
+def from_config(config_path):
+    with open(config_path) as f:
+        config = json.load(f)
+
+    mongo_url = config['mongo_url']
+
+    connect(mongo_url, alias='metadata')
+
