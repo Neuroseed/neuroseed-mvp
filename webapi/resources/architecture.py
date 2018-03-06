b7333c306ef294fc89bbdea6b9c976094f28f669
Ihor Omelchenko <counter3d@gmail.com>
2018-03-06 18:13:18 +0200

    ADD: POST architecture - create new architecture

--- webapi/resources/architecture.py
+++ webapi/resources/architecture.py
@@ -1,4 +1,9 @@
+import uuid
 import falcon
+from falcon.media.validators import jsonschema
+
+import metadata
+from .schema import ARCHITECTURE_SCHEMA
 
 __all__ = [
     'ArchitectureResource'
@@ -26,13 +31,31 @@ class ArchitectureResource:
 
     def on_post(self, req, resp, id=None):
         if id:
-            self.create_architecture(req, resp, id)
+            self.set_architecture(req, resp, id)
         else:
-            self.init_architecture(req, resp)
+            self.create_architecture(req, resp)
 
-    def create_architecture(self, req, resp, id):
-        pass
+    @jsonschema.validate(ARCHITECTURE_SCHEMA)
+    def set_architecture(self, req, resp, id):
+        architecture = metadata.Architecture.objects.get({'id': id})
+        architecture._set_attributes(req.media)
+        architecture.save()
 
-    def init_architecture(self, req, resp):
-        pass
+        resp.media = {
+            'success': True
+        }
+
+    @jsonschema.validate(ARCHITECTURE_SCHEMA)
+    def create_architecture(self, req, resp):
+        req.media['_id'] = req.media['id']  # костыль
+        del req.media['id']  # костыль
+
+        architecture = metadata.Architecture.from_document(req.media)
+        architecture.id = req.media.get('_id', None) or uuid.uuid4()
+        architecture.save()
+
+        resp.media = {
+            'success': True,
+            'id': architecture.id
+        }
 
--- webapi/resources/schema.py
+++ webapi/resources/schema.py
@@ -9,6 +9,33 @@ schema = {
 "required":["title"]
 }
 
+ARCHITECTURE_SCHEMA = {
+    "type": "object",
+    "properties": {
+        "id": {"type": "string"},
+        "is_public": {"type": "boolean"},
+        "title": {
+            "type": "string",
+            "minLength": 1,
+            "maxLength": 128
+        },
+        "description": {
+            "type": "string",
+            "maxLength": 128
+        },
+        "category": {
+            "type": "string",
+            "maxLength": 128
+        },
+        "architecture": {
+            "type": "object",
+            "properties": {
+                "layers": {"type": "array"}
+            }
+        }
+    }
+}
+
 MODEL_SCHEMA = {
     "is_public": {"type": "boolean"},
     "meta":{
