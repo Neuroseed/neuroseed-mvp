78315ed12f2ee4c604cdfd58b3aab5b1c17d97b4
Ihor Omelchenko <counter3d@gmail.com>
2018-03-21 12:46:59 +0200

    Add worker config hierarchy

--- celery_worker.py
+++ celery_worker.py
@@ -1,19 +1,5 @@
-import sys
-import json
-
-import metadata
-import storage
 import worker
-from worker.app import *
 
-metadata.from_config('config/metadata_config.json')
-storage.from_config('config/storage_config.json')
-worker.from_config('config/celery_config.json')
 
-with open('config/worker_config.json') as f:
-        config = json.load(f)
-
-log_level = config['log_level']
-sys.argv.extend(['-l', log_level])
-sys.argv.append('--autoscale=10,1')
-app.worker_main()
+worker.from_config('config/worker_config.json')
+worker.main()
