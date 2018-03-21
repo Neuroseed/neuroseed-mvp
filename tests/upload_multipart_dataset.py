98e4cad0e48d185c0a20eef5ae71e8072a0a69d8
Ihor Omelchenko <counter3d@gmail.com>
2018-03-21 18:22:28 +0200

    Add example upload multipart

--- tests/upload_multipart_dataset.py
+++ tests/upload_multipart_dataset.py
@@ -12,15 +12,13 @@ payload = {
 TOKEN = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
 
 SEND_FILE = 'tests/large_dataset.hdf5'
-DATASET_ID = 'b54a6da7-7e87-473a-9d93-8f3e6f8a0864'
-URL = 'http://localhost:8080/api/v1/dataset/{}'.format(DATASET_ID)
 
 
 def create_hdf5(file_name):
     f = h5py.File(file_name, 'w')
 
     # float size 4 bytes
-    n = 10**4
+    n = 10**8
     dataset = f.create_dataset('dataset', (n,), dtype='f')
 
     dataset[...] = numpy.ones(n)
@@ -28,7 +26,31 @@ def create_hdf5(file_name):
     f.close()
 
 
-def upload(file_name):
+def create_dataset_metadata():
+    url = 'http://localhost:8080/api/v1/dataset'
+
+    dataset_meta = {
+        "is_public": False,
+        "title": "Test Dataset123",
+        "description": "Test test",
+        "category": "classification",
+    }
+
+    headers = {
+        'Authorization': 'Bearer {token}'.format(token=TOKEN)
+    }
+
+    r = requests.post(url, json=dataset_meta, headers=headers)
+
+    print('Create dataset metadata:', r.status_code, 'data:', r.text)
+
+    if r.status_code == 200:
+        return r.json()['id']
+
+
+def upload(dataset_id, file_name):
+    url = 'http://localhost:8080/api/v1/dataset/{}'.format(dataset_id)
+
     with open(file_name, 'rb') as f:
         form = encoder.MultipartEncoder({
             "file": (file_name, f, "text/plain")
@@ -41,12 +63,13 @@ def upload(file_name):
             'Authorization': 'Bearer {token}'.format(token=TOKEN)
         }
 
-        resp = requests.post(URL, headers=headers, data=form, stream=True)
+        resp = requests.post(url, headers=headers, data=form, stream=True)
 
-        print('Status code:', resp.status_code, 'resp.text:', resp.text)
+        print('Upload dataset:', resp.status_code, 'resp.text:', resp.text)
 
 
 if __name__ == '__main__':
     create_hdf5(SEND_FILE)
-    upload(SEND_FILE)
+    id = create_dataset_metadata()
+    upload(id, SEND_FILE)
 
