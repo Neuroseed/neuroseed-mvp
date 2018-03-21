cbce3d59b6d264cb41150f51021d427bbc2614d6
Ihor Omelchenko <counter3d@gmail.com>
2018-03-21 15:31:51 +0200

    Fix worker model train hdf5 datasets names

--- worker/tasks/__init__.py
+++ worker/tasks/__init__.py
@@ -1,8 +1,3 @@
 from .train_model import *
+from .test_model import *
 from .predict_model import *
-
-
-@app.task(bind=True, name='model.test')
-def test_model(self):
-    task_id = self.request.id
-    return {'id': task_id}
