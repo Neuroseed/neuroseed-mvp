ed7da9a881c62d5b5c852421e8b6f36f567bf5e2
Stanislav <stas.kozhemyako@gmail.com>
2018-03-22 18:24:22 +0200

    add Dataset test

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -2,33 +2,71 @@ import falcon
 from falcon import testing
 import unittest
 import webapi
+import uuid
 import metadata
+import jwt
 from mongoengine import connect
 
+
 class TestIntAPI(testing.TestCase):
+    def create_token(self, user_id):
+        payload = {
+            'user_id': user_id
+        }
+        TOKEN = jwt.encode(payload, self.SECRET_KEY, algorithm='HS256').decode('utf-8')
+    
+    def get_auth_headers(self, token):
+        headers = {'Authorization': 'Bearer {token}'.format(token=token)}
+        return headers
+
     def setUp(self):
         super().setUp()
-        
         connect('metaddata', host='mongomock://localhost', alias='metadata')
         config = {
             "auth_key_file": "config/auth.key",
             "celery_config": "config/celery_config.json",
             "metadata_config": {},
         }
+        self.SECRET_KEY = open(config["auth_key_file"]).read()
         self.app = webapi.main(config)
 
+
 class TestDatasets(TestIntAPI):
-    def test_configure_api_v1(self):
-        body= {'ids': []}
-        result = self.simulate_get('/api/v1/datasets')
-        self.assertEqual(result.json, body)
+    def setUp(self):
+        super().setUp()
+        document = {
+            'is_public': True
+            }
+        dataset_meta = metadata.DatasetMetadata(**document)
+        dataset_meta.id = str(uuid.uuid4())
+        dataset_meta.url = dataset_meta.id
+        dataset_meta.base.owner = "user_id"
+        dataset_meta.save()
+        self.dataset_meta = dataset_meta
 
-    def test_configure_api_v2(self):
+    def tearDown(self):
+        metadata.DatasetMetadata.objects.all().delete()
+    
+    def test_get_datasets(self):
+        body= {'ids': [self.dataset_meta.id]}
+        token = self.create_token("user_id")
+        headers = self.get_auth_headers(token)
+        result = self.simulate_get('/api/v1/datasets', headers=headers)
+        self.assertEqual(result.json, body)
+    
+
+    def test_get_dataset_id(self):
         body= {'error': 'Dataset metadata doesnt exist'}
-        result = self.simulate_get('/api/v1/dataset/{id}')
-        self.assertEqual(result.json, body)
+        token = self.create_token("user_id")
+        headers = self.get_auth_headers(token)
+        result = self.simulate_get('/api/v1/dataset/{id}'.format(id=self.dataset_meta.id), headers=headers)
+        self.assertEqual(result.status, falcon.HTTP_200)
 
+
+'''
 class TestArchitecture(TestIntAPI):
+    def setUp(self):
+        super().setUp()
     def test_configure_api_v3(self):
         body= {'ids': []}
         result = self.simulate_get('/api/v1/architectures')
@@ -38,3 +76,5 @@ class TestArchitecture(TestIntAPI):
         body= {'error': 'Architecture does not exist'}
         result = self.simulate_get('/api/v1/architecute/{id}')
         self.assertEqual(result.status, falcon.HTTP_404)
+
+'''
