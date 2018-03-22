6b1955e18ced06360013de37ba0565c84ecc76de
Ihor Omelchenko <counter3d@gmail.com>
2018-03-22 15:59:29 +0200

    Fix web api models auth

--- webapi/apiv1/resources/model/models.py
+++ webapi/apiv1/resources/model/models.py
@@ -1,3 +1,5 @@
+import logging
+
 import falcon
 import metadata
 
@@ -5,15 +7,22 @@ __all__ = [
     'ModelsResource'
 ]
 
+logger = logging.getLogger(__name__)
+
 
 class ModelsResource:
     def on_get(self, req, resp):
-        models_meta = metadata.ModelMetadata.objects.all()
+        user_id = req.context['user']
+        logger.debug('Authorize user {id}'.format(id=user_id))
 
-        ids = [meta.id for meta in models_meta]
+        models = metadata.ModelMetadata.objects(is_public=True)
+        ids = [meta.id for meta in models]
+
+        if user_id:
+            models = metadata.ModelMetadata.objects(is_public=False, owner=user_id)
+            ids = [meta.id for meta in models] + ids
 
         resp.status = falcon.HTTP_200
         resp.media = {
             'ids': ids  # response schema v0.2.1
         }
-
