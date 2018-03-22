2653f45eaa8e59f4f9d690d3c8bf081aa99e9215
Ihor Omelchenko <counter3d@gmail.com>
2018-03-22 15:26:21 +0200

    Fix web api architectures auth

--- webapi/apiv1/resources/architectures.py
+++ webapi/apiv1/resources/architectures.py
@@ -1,3 +1,5 @@
+import logging
+
 import falcon
 import metadata
 
@@ -5,12 +7,20 @@ __all__ = [
     'ArchitecturesResource'
 ]
 
+logger = logging.getLogger(__name__)
+
 
 class ArchitecturesResource:
     def on_get(self, req, resp):
-        architectures = metadata.ArchitectureMetadata.objects.all()
+        user_id = req.context['user']
+        logger.debug('Authorize user {id}'.format(id=user_id))
 
-        ids = [arch.id for arch in architectures]
+        architectures = metadata.ArchitectureMetadata.objects(is_public=True)
+        ids = [meta.id for meta in architectures]
+
+        if user_id:
+            architectures = metadata.ArchitectureMetadata.objects(is_public=False, owner=user_id)
+            ids = [meta.id for meta in architectures] + ids
 
         resp.status = falcon.HTTP_200
         resp.media = {
