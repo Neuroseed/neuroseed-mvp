9d70eec5606c7696dee467e4a5428bea9d6f711f
Ihor Omelchenko <counter3d@gmail.com>
2018-03-23 17:48:55 +0200

    Add models full/number tests

--- webapi/apiv1/resources/model/models.py
+++ webapi/apiv1/resources/model/models.py
@@ -39,12 +39,11 @@ class ModelsFullResource:
         models_meta = self.get_models_meta(models)
 
         if user_id:
-            models = metadata.DatasetMetadata.objects(is_public=False, base__owner=user_id)
+            models = metadata.ModelMetadata.objects(is_public=False, base__owner=user_id)
             models_meta = self.get_models_meta(models) + models_meta
 
         from_ = int(req.params.get('from', 0))
         number = int(req.params.get('number', 99999))
-        print('params:', from_, number)
         if from_ < len(models_meta):
             models_meta = models_meta[from_: from_ + number]
 
