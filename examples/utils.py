8e4e9e561d5b93c3c1c70c7bd14742b3115840cb
Stanislav <stas.kozhemyako@gmail.com>
2018-03-27 13:40:05 +0300

    Add upload keras image datasets

--- examples/utils.py
+++ examples/utils.py
@@ -1,7 +1,7 @@
 import os
 import requests
 import jwt
-
+from requests_toolbelt.multipart import encoder
 
 def get_auth_header():
     here = os.path.abspath(os.path.dirname(__file__))
@@ -19,6 +19,23 @@ def get_auth_header():
     return {
         'Authorization': 'Bearer {token}'.format(token=token)
     }
+
+
+def upload(dataset_id, file_name):
+    url = 'http://localhost:8080/api/v1/dataset/{}'.format(dataset_id)
+    with open(file_name, 'rb') as f:
+        form = encoder.MultipartEncoder({
+            "file": (file_name, f, "text/plain")
+        })
+        print("Content-Type:", form.content_type)
+            
+        headers = {
+            "Prefer": "respond-async",
+            "Content-Type": form.content_type,
+        }
+            
+        resp = post(url, headers=headers, data=form, stream=True)
+    print('Upload dataset:', resp.status_code, 'resp.text:', resp.text)
 
 
 def get(*args, **kwargs):
