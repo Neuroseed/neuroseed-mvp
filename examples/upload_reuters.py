202be18306ad686a417535fd6aaa1f70439ec9d2
Stanislav <stas.kozhemyako@gmail.com>
2018-03-29 15:38:26 +0300

    Upload text datasets≈

--- examples/upload_reuters.py
+++ examples/upload_reuters.py
@@ -1,18 +1,51 @@
+import os
+import requests
+import jwt
+import utils
 import numpy
 import h5py
 import keras
 from keras.preprocessing import sequence
 from keras.datasets import reuters
 
+SECRET_KEY = 'secret'
+payload = {'user_id': 'user-user-user'}
+
+TOKEN = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+
 hdf5_file = 'reuters.hdf5'
 batch_size = 32
 num_classes = 46
 path = 'reuters_word_index.json'
 
+def create_dataset_meta():
+    url = 'http://localhost:8080/api/v1/dataset'
+    
+    dataset_meta = {
+        "is_public": True,
+        "title": "Reuters newswire topics",
+        "description": "Dataset of 11,228 newswires from Reuters, labeled over 46 topics. As with the IMDB dataset, each wire is encoded as a sequence of word indexes (same conventions).",
+        "category": "classification"
+    }
+    
+    headers = {
+        'Authorization': 'Bearer {token}'.format(token=TOKEN)
+    }
+    r = requests.post(url, json=dataset_meta, headers=headers)
+    print('Create dataset metadata: ', r.status_code, 'data:', r.text)
 
-def imdb_to_hdf5(file_name):
+    if r.status_code == 200:
+        return r.json()['id']
+    
+    raise RuntimeError('Status code', r.status_code, 'text:', r.text)
+
+
+def reuters_to_hdf5(file_name):
+    if os.path.exists(file_name):
+        return
+
     (x_train, y_train), (x_test, y_test) = reuters.load_data(num_words=10000, maxlen=500)
-#    word_index = imdb.get_word_index(path="reuters_word_index.json")
+    word_index = reuters.get_word_index(path="reuters_word_index.json")
     print('x_train shape:', x_train.shape)
     print(x_train.shape[0], 'train samples')
     print(x_test.shape[0], 'test samples')
@@ -22,18 +55,20 @@ def imdb_to_hdf5(file_name):
 
     x_train = x_train.astype('object')
     x_test = x_test.astype('object')
-#    word_index = word_index.astype('string')
 
     x = numpy.concatenate((x_train, x_test), axis=0)
     x = sequence.pad_sequences(x, maxlen=400)
     y = numpy.concatenate((y_train, y_test), axis=0)
     y = sequence.pad_sequences(y, maxlen=400)
-#    word_index = h5py.spetial_dtype(vlen=str)
+
 
     with h5py.File(file_name, 'w') as f:
         f.create_dataset('x',data=x, compression='gzip')
         f.create_dataset('y',data=y, compression='gzip')
-#        word_index = f.create_group(('/dict'); for key in dict: word_index[key] = dict[key])
-
+        dictionary = f.create_group('dictionary')
+        for key in word_index:
+            dictionary[key] = word_index[key]
 if __name__ == '__main__':
-    imdb_to_hdf5(hdf5_file)
+    reuters_to_hdf5(hdf5_file)
+    id = create_dataset_meta()
+    utils.upload(id, hdf5_file)
