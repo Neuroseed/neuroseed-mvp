5baa4100c3fc78ed06b3cd8cfeb8879e5e3f84bf
Ihor Omelchenko <counter3d@gmail.com>
2018-04-02 14:30:59 +0300

    Add metods to flatten in database metadata

--- manager/task.py
+++ manager/task.py
@@ -1,31 +1,8 @@
 import uuid
 
 import metadata
+from metadata import TaskMetadata
 from celery.app import control
-
-
-class Task(metadata.TaskMetadata):
-    def to_dict(self):
-        meta = self.to_mongo().to_dict()
-
-        if '_id' in meta:
-            meta['id'] = meta['_id']
-            del meta['_id']
-
-        del meta['_cls']
-
-        return meta
-
-    def from_dict(self, meta):
-        for name in self._fields:
-            setattr(self, name, meta[name])
-
-
-def get_tasks_ids():
-    tasks = Task.objects.all()
-    ids = [task.id for task in tasks]
-
-    return ids
 
 
 def terminate(task_id):
