b7de2a46d8d016a3770a35fa3ef93aa373aed530
Ihor Omelchenko <counter3d@gmail.com>
2018-04-03 15:52:39 +0300

    Move error serializer to webapi/serializers.py

--- webapi/__init__.py
+++ webapi/__init__.py
@@ -13,6 +13,7 @@ import storage
 from . import apiv1
 from .authmiddleware import NeuroseedAuthMiddleware
 from .loggingmiddleware import LoggingMidleware
+from . import serializers
 
 # fix falcon bug
 from . import patch
@@ -54,45 +55,41 @@ def init_logging():
     logging.warning('Start logging')
 
 
-def from_config(config_file):
+def from_config(config_file=None):
     if type(config_file) is str:
         with open(config_file) as f:
             config = json.load(f)
     elif type(config_file) is dict:
         config = config_file
+    elif config_file is None:
+        return {}
 
-    metadata_config = config['metadata_config']
-    metadata.from_config(metadata_config)
+    metadata_config = config.get('metadata_config', None)
+    if metadata_config:
+        metadata.from_config(metadata_config)
 
-    storage_config = config['storage_config']
-    storage.from_config(storage_config)
+    storage_config = config.get('storage_config', None)
+    if storage_config:
+        storage.from_config(storage_config)
 
     return config
 
 
-def falcon_error_serializer(_: falcon.Request, resp:falcon.Response, exc: falcon.HTTPError) -> None:
-    """ Serializer for native falcon HTTPError exceptions.
+def get_auth_key(key_file):
+    with open(key_file) as f:
+        secret_key = f.read().strip()
 
-    Serializes HTTPError classes as proper json:api error
-        see: http://jsonapi.org/format/#errors
-    """
-    error = {
-        'title': exc.title,
-        'error': exc.description,
-        'status': int(exc.status[0:3]),
-    }
-
-    if hasattr(exc, "link") and exc.link is not None:
-        error['links'] = {'about': exc.link['href']}
-
-    resp.body = json.dumps(error)
+    return secret_key
 
 
 def main(config):
-    key_file = config['auth_key_file']
+    if not type(config) is dict:
+        raise TypeError('type of config must be dict')
 
-    with open(key_file) as f:
-        secret_key = f.read().strip()
+    key_file = config['auth_key_file']
+    secret_key = get_auth_key(key_file)
+
+    middlewares = []
 
     user_loader = lambda payload: payload['user_id']
     jwt_auth_backend = JWTAuthBackend(
@@ -101,25 +98,26 @@ def main(config):
         required_claims=['user_id'],
         auth_header_prefix='Bearer')
     auth_middleware = NeuroseedAuthMiddleware(jwt_auth_backend)
+    middlewares.append(auth_middleware)
 
     # Cross-origin resource sharing - "совместное использование ресурсов между разными источниками"
-    cors = CORS(
-        allow_all_origins=True,
-        allow_credentials_all_origins=True,
-        allow_all_methods=True,
-        allow_all_headers=True)
+    use_cors = config.get('use_cors', True)
+    if use_cors:
+        cors = CORS(
+            allow_all_origins=True,
+            allow_credentials_all_origins=True,
+            allow_all_methods=True,
+            allow_all_headers=True)
+        middlewares.append(cors.middleware)
 
-    logging_middleware = LoggingMidleware(logger)
+    use_extend_logging = config.get('use_extend_logging', True)
+    if use_extend_logging:
+        logging_middleware = LoggingMidleware(logger)
+        middlewares.append(logging_middleware)
 
-    middleware = [
-        auth_middleware,
-        logging_middleware,
-        cors.middleware
-    ]
+    api = falcon.API(middleware=middlewares)
 
-    api = falcon.API(middleware=middleware)
-
-    api.set_error_serializer(falcon_error_serializer)
+    api.set_error_serializer(serializers.falcon_error_serializer)
 
     apiv1.configure_api_v1(api, auth_middleware)
 
