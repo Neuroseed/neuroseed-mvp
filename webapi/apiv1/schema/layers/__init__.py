bce4e9018ce3b3d1f3ab0529f26c6dd23550bb59
Stanislav <stas.kozhemyako@gmail.com>
2018-04-04 14:55:37 +0300

    Add flatten lay schema, edit __init__

--- webapi/apiv1/schema/layers/__init__.py
+++ webapi/apiv1/schema/layers/__init__.py
@@ -3,11 +3,13 @@ from .conv2d import CONV2D_LAY
 from .dropout import DROPOUT_LAY
 from .averagepooling2d import AVERAGEPOOLING2D_LAY
 from .maxpooling2d import MAXPOOLING2D_LAY
+from .flatten import FLATTEN_LAY
 
 LAYERS = [
           DENSE_LAY,
           CONV2D_LAY,
           DROPOUT_LAY,
           AVERAGEPOOLING2D_LAY,
-          MAXPOOLING2D_LAY
-          ]
+          MAXPOOLING2D_LAY,
+          FLATTEN_LAY
+]
