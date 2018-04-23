72cd728f8bbca50ea5578fc60a686fff62b9d9ea
Stanislav <stas.kozhemyako@gmail.com>
2018-04-23 16:06:58 +0300

    Replase --net=host -> -p

--- docker/start-mongo-test.sh
+++ docker/start-mongo-test.sh
@@ -1,1 +1,1 @@
-docker run -d --rm --net=host --name mongo mongo:3.6.0
+docker run --rm -d -p 27017:27017 --name mongo mongo:3.6.0
--- docker/start-rabbitmq-test.sh
+++ docker/start-rabbitmq-test.sh
@@ -1,4 +1,4 @@
-docker run -d --rm --net=host \
+docker run --rm -d -p 5671:5671 -p 5672:5672 \
 -v $PWD/config/rabbitmq/rabbitmq.conf:/etc/rabbitmq/rabbitmq.conf \
 -v $PWD/config/rabbitmq/cert.cer:/etc/cert.cer \
 -v $PWD/config/rabbitmq/key.pem:/etc/key.pem \
